1bbf1268020b36cde579eefc41ba3dbc3de5bf0e
Anastasia2112 <nastia.ttn@gmail.com>
2022-06-14 22:07:40 +0300

    hot fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const ApiError = require('../error/ApiError');
 const bcrypt = require('bcrypt');
-const {User, Basket} = require('../models/models')
+const { User, Basket } = require('../models/models')
 
 class UserController {
     async registration(req, res) {
