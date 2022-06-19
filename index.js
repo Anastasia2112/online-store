6aa5fad0b16930502af4125c36ac8d0a5a61b21c
Anastasia2112 <nastia.ttn@gmail.com>
2022-06-20 00:23:30 +0300

    interaction with the server

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const sequelize= require('./db')
 const models = require('./models/models')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
-const router = require('./routes/index')
-const errorHandler = require('./middleware/ErrorHandlingMiddleware')
-const path = require('path')
+const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000
 
